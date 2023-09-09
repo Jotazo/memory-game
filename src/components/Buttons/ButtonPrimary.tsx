94958314197d165ffd85a4702fd1c5ae1392c9fb
Javier Guerrero <javigh87@gmail.com>
2023-09-09 16:59:14 +0200

    refactor: ButtonPrimary added classStyles props

--- src/components/Buttons/ButtonPrimary.tsx
+++ src/components/Buttons/ButtonPrimary.tsx
@@ -1,8 +1,15 @@
+import { ButtonHTMLAttributes } from "react";
 import Button from "./Button";
 
-const ButtonPrimary = ({ text }: { text: string }) => {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  classStyles?: string;
+  active?: boolean;
+}
+
+const ButtonPrimary: React.FC<ButtonProps> = ({ text, classStyles }) => {
   const primaryStyles = "bg-orange hover:bg-[#FFB84A] text-softWhite";
-  return <Button text={text} classStyles={primaryStyles} />;
+  return <Button text={text} classStyles={`${primaryStyles} ${classStyles}`} />;
 };
 
 export default ButtonPrimary;
