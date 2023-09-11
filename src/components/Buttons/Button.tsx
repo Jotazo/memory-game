bf01c5da4bb00841244c15fe8e6530a847f958f1
Javier Guerrero <javigh87@gmail.com>
2023-09-11 09:17:01 +0200

    fix: added fit prop Button

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -2,17 +2,21 @@ import { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  fit?: boolean;
   classStyles?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
   text,
+  fit,
   classStyles,
   ...props
 }) => {
   return (
     <button
-      className={`w-full transition text-lg sm:text-xl px-7 py-[10px] rounded-full tracking-wide ${classStyles}`}
+      className={`${
+        fit ? "w-fit" : "w-full"
+      } transition text-lg sm:text-xl px-7 py-[10px] rounded-full tracking-wide ${classStyles}`}
       {...props}
     >
       {text}
--- src/components/Buttons/ButtonPrimary.tsx
+++ src/components/Buttons/ButtonPrimary.tsx
@@ -3,13 +3,20 @@ import Button from "./Button";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  fit?: boolean;
   classStyles?: string;
   active?: boolean;
 }
 
-const ButtonPrimary: React.FC<ButtonProps> = ({ text, classStyles }) => {
+const ButtonPrimary: React.FC<ButtonProps> = ({ text, fit, classStyles }) => {
   const primaryStyles = "bg-orange hover:bg-[#FFB84A] text-softWhite";
-  return <Button text={text} classStyles={`${primaryStyles} ${classStyles}`} />;
+  return (
+    <Button
+      text={text}
+      fit={fit}
+      classStyles={`${primaryStyles} ${classStyles}`}
+    />
+  );
 };
 
 export default ButtonPrimary;
