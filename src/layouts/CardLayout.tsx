76750aa6a986e6d424261b35f8f412772e0d556a
Javier Guerrero <javigh87@gmail.com>
2023-09-12 08:00:20 +0200

    refactor: CardLayout added customCssClasses to props

--- src/layouts/CardLayout.tsx
+++ src/layouts/CardLayout.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const CardLayout = ({ children }: { children: React.ReactNode }) => {
+const CardLayout = ({
+  children,
+  customClasses = "",
+}: {
+  children: React.ReactNode;
+  customClasses?: string;
+}) => {
   return (
-    <div className="bg-softWhite p-6 md:p-12 rounded-xl w-[90%] max-w-xl">
+    <div
+      className={`bg-softWhite p-6 md:p-12 rounded-xl w-[90%] max-w-xl ${customClasses}`}
+    >
       {children}
     </div>
   );
