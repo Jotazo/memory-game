459826dcfae03c3531c8869d07b652702ffb1567
Javier Guerrero <javigh87@gmail.com>
2023-09-13 13:08:55 +0200

    feature: Added functionality header menu button

--- src/pages/GameScreen/Header.tsx
+++ src/pages/GameScreen/Header.tsx
@@ -1,10 +1,18 @@
 import { ButtonPrimary } from "../../components/Buttons";
+import { Modals } from "../../enums";
+import { useMemoryGameStore } from "../../store/useMemoryGameStore";
 
 const Header = () => {
+  const showModal = useMemoryGameStore((state) => state.showModal);
   return (
     <header className="flex justify-between items-center">
       <h3 className="text-2xl">memory</h3>
-      <ButtonPrimary fit text="Menu" classStyles="ml-auto w-fit" />
+      <ButtonPrimary
+        onClick={() => showModal(Modals.Menu)}
+        fit
+        text="Menu"
+        classStyles="ml-auto w-fit"
+      />
     </header>
   );
 };
