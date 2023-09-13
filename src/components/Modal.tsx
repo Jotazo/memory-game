b979ab8a8eba063faf3517dfe728e0ad3f8b3e3b
Javier Guerrero <javigh87@gmail.com>
2023-09-13 13:13:08 +0200

    feature: Added animation to Modal show and hide

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,16 @@
+import { motion } from "framer-motion";
+
 const Modal = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="absolute top-0 bottom-0 right-0 left-0 flex justify-center items-center"
+    >
       <div className="absolute top-0 bottom-0 right-0 left-0 bg-zinc-950 opacity-70"></div>
       {children}
-    </div>
+    </motion.div>
   );
 };
 
