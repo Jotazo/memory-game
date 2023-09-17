0d6ea7c76cd8e75b98a9591d0f52b1cc124402bf
Javier Guerrero <javigh87@gmail.com>
2023-09-17 06:59:40 +0200

    refactor: Removing onlyMobile

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { AnimatePresence } from "framer-motion";
 
 import { useMemoryGameStore } from "./store/useMemoryGameStore";
 
-import useMobile from "./hooks/useMobile";
-
 import { Modals } from "./enums";
 
-import { MainScreen, GameScreen, OnlyMobile } from "./pages";
+import { MainScreen, GameScreen } from "./pages";
 
 import Modal from "./components/Modal";
 import Menu from "./components/Modals/Menu";
@@ -14,34 +12,29 @@ import EndGameSolo from "./components/Modals/EndGameSolo";
 import EndGameMulti from "./components/Modals/EndGameMulti";
 
 function App() {
-  const isMobile = useMobile();
   const gameStarted = useMemoryGameStore((state) => state.gameStarted);
   const modalToShow = useMemoryGameStore((state) => state.modalToShow);
 
   return (
     <div className="font-atkinson h-screen">
-      {!isMobile ? (
-        <OnlyMobile />
-      ) : (
-        <AnimatePresence>
-          {gameStarted ? (
-            <GameScreen key="gameScreen" />
-          ) : (
-            <MainScreen key="mainScreen" />
-          )}
-          {modalToShow && (
-            <Modal>
-              {modalToShow === Modals.Menu && <Menu key={Modals.Menu} />}
-              {modalToShow === Modals.GameSolo && (
-                <EndGameSolo key={Modals.GameSolo} />
-              )}
-              {modalToShow === Modals.GameMulti && (
-                <EndGameMulti key={Modals.GameMulti} />
-              )}
-            </Modal>
-          )}
-        </AnimatePresence>
-      )}
+      <AnimatePresence>
+        {gameStarted ? (
+          <GameScreen key="gameScreen" />
+        ) : (
+          <MainScreen key="mainScreen" />
+        )}
+        {modalToShow && (
+          <Modal>
+            {modalToShow === Modals.Menu && <Menu key={Modals.Menu} />}
+            {modalToShow === Modals.GameSolo && (
+              <EndGameSolo key={Modals.GameSolo} />
+            )}
+            {modalToShow === Modals.GameMulti && (
+              <EndGameMulti key={Modals.GameMulti} />
+            )}
+          </Modal>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
